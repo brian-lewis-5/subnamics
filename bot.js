5045ea5f8bf3ae50b65752d182f57aaa718181bc
nicolasmoise <nicolas.moise@gmail.com>
2017-02-24 15:19:31 -0500

    update token 2

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ const botBuilder = require('claudia-bot-builder'),
   rp = require('minimal-request-promise'),
   fbTemplate = botBuilder.fbTemplate,
   format = text => (text && text.substring(0, 80)),
-  apiToken = '{"sig_field":"TestCust","ts":"1487966047","sig":"jwUcX6sqeaoxVmjY4l7o6MKREfFAnJY5O81CWe8jZN8=","public_id":"0e5de2bedc5e11e3a2e4bc764e106cf4"}',
-  queryString = '?merchant_id=0e5de2bedc5e11e3a2e4bc764e106cf4&merchant_user_id=TestCust&public_id=0e5de2bedc5e11e3a2e4bc764e106cf4&sig=jwUcX6sqeaoxVmjY4l7o6MKREfFAnJY5O81CWe8jZN8%3D&sig_field=TestCust&ts=1487966047';
+  apiToken = '{"sig_field":"TestCust","ts":"1487967506","sig":"4EQNU6YKg8Ksa8apI6CmrmiuRq4tm4zBPWh6mAN+LRE=","public_id":"0e5de2bedc5e11e3a2e4bc764e106cf4"}',
+  queryString = '?merchant_id=0e5de2bedc5e11e3a2e4bc764e106cf4&merchant_user_id=TestCust&public_id=0e5de2bedc5e11e3a2e4bc764e106cf4&sig=4EQNU6YKg8Ksa8apI6CmrmiuRq4tm4zBPWh6mAN%2BLRE%3D&sig_field=TestCust&ts=1487967506';
 
 function doReceipt() {
     return new fbTemplate.Receipt('Gregory Mueller', '12345678902', 'USD', 'Visa 2345')
