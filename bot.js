e93f625682637f892feef6b91078ee11870f412b
Brian Lewis <brian.lewis@ordergroove.com>
2017-02-24 10:22:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,84 +7,6 @@ const botBuilder = require('claudia-bot-builder'),
   fbTemplate = botBuilder.fbTemplate,
   format = text => (text && text.substring(0, 80));
 
-// function doWelcome() {
-//     const generic = new fbTemplate.generic();
-
-//     generic
-//         .addBubble(format('Hi, there, I\'m Rosie. Zworp.'), format('I can help get the cat food or toilet paper you\'re looking for.'))
-//         .addImage('http://i.imgur.com/twTukoI.jpg')
-//         .addButton('I need cat food', 'I need cat food')
-//         .addButton('I need tp', 'I need tp')
-//         .addButton('Check My Orders', 'My Orders');
-
-//     return generic.get();
-// }
-
-function doINeedCatFood() {
-    const generic = new fbTemplate.generic();
-
-    generic
-        .addBubble(format('Zwee, we\'ve got plenty of cat food'), format('What do you look for in cat food?'))
-        .addButton('I need organic', 'I need organic')
-        .addButton('I need high fish content', 'I need high fish content')
-        .addButton('I need non-smelly', 'I need non-smelly');
-
-    return generic.get();
-}
-
-function doINeedTp() {
-    const generic = new fbTemplate.generic();
-
-    generic
-        .addBubble(format('Zwee, we\'ve got plenty of toilet paper'), format('What do you look for in toilet paper?'))
-        .addButton('I need luxury', 'I need luxury')
-        .addButton('I need economical', 'I need economical')
-        .addButton('I need dolphin print', 'I need dolphin print');
-
-    return generic.get();
-}
-
-function doINeedDolphinPrintTp() {
-    const generic = new fbTemplate.generic();
-
-    generic
-        .addBubble(format('Dolphin Print Toilet Paper'), format('100% Soft and Luxurious Cotton, with fun dolphin print'))
-        .addImage('http://i.imgur.com/S4A7gwO.png')
-        .addButton('Order', 'Order dolphin')
-        .addButton('Subscribe', 'Subscribe dolphin')
-        .addButton('Do something else', 'Other dolphin');
-
-    return generic.get();
-}
-
-function doQuantityDolphinPrintTp() {
-	return 'How many Dolphin Print Toilet Papers would you like us to send?';
-}
-
-function doFrequencyDolphinPrintTp() {
-	const generic = new fbTemplate.generic();
-
-    generic
-        .addBubble(format('How often would you like to receive Dolphin Print Toilet Paper?'))
-        .addButton('Every week', 'Every week')
-        .addButton('Every month', 'Every month')
-        .addButton('Every other month', 'Every other month');
-
-    return generic.get();
-}
-
-function doCheckoutDolphinPrintTp() {
-    const generic = new fbTemplate.generic();
-
-    generic
-        .addBubble()
-        .addImage('http://i.imgur.com/ErRkNHI.png')
-        .addButton('Pay', 'Pay dolphin')
-        .addButton('Cancel', 'Cancel dolphin');
-
-    return generic.get();
-}
-
 function doReceiptDolphinPrintTp() {
     return new fbTemplate.Receipt('Steve\'s Mom', '12345678902', 'USD', 'Visa 2345')
         .addTimestamp(new Date(1428444852))
@@ -171,25 +93,39 @@ function getOrderDetails(id) {
 }
 
 function doWelcome() {
-	const generic = new fbTemplate.generic();
-
-	generic.addImage('http://i.imgur.com/twTukoI.jpg');
-	return generic.get();
-	// const generic = new fbTemplate.generic();
-
- //    generic
- //        .addBubble(format('Hi, there, I\'m Rosie. Zworp.'), format('I can help get the cat food or toilet paper you\'re looking for.'))
- //        .addImage('http://i.imgur.com/twTukoI.jpg')
- //        .addButton('I need cat food', 'I need cat food')
- //        .addButton('I need tp', 'I need tp')
- //        .addButton('Check My Orders', 'My Orders');
-
- //    return generic.get();
+    return new fbTemplate.generic()
+        .addBubble(format('Welcome back, Gregory.'), format('Begin by either typing the name of a product or tapping "Check my orders"'))
+        .addImage('http://i.imgur.com/twTukoI.jpg')
+        .addButton('Check my orders', 'check orders')
+        .get();
 }
 
+function doChooseTp() {
+    return new fbTemplate.generic()
+        .addBubble(format('Choose a description of the toilet paper you\'re after'))
+        .addButton('Luxurious', 'luxury tp')
+        .addButton('Economical', 'economical tp')
+        .addButton('Fun', 'fun tp')
+        .get();
+}
+
+function doFunTp() {
+    return new fbTemplate.generic()
+        .addBubble(format('Great! I found fun toilet paper. You can either place an order or subscribe'))
+        .addButton('Place order', 'order tp')
+        .addButton('Subscribe', 'subscribe tp')
+        .get();
+}
+
+function doChooseShipping() {
+    return 'egg';
+    // return new fbTemplate.generic()
+    //     .addBubble(format('Where should I send it? Type an address or tap "share location"'))
+    //     .get();
+}
 
 const bot = botBuilder((request, apiReq) => {
-	const msg = request.text.toLowerCase();
+  const msg = request.text.toLowerCase();
 
     apiReq.lambdaContext.callbackWaitsForEmptyEventLoop = false
 
@@ -197,48 +133,24 @@ const bot = botBuilder((request, apiReq) => {
       return getOrderDetails(request.text.slice('ORDER_DETAILS_'.length));
     }
 
-	switch(true) {
-		case(msg === 'hi' || msg === 'hello'):
-			return doWelcome();
-		case(msg === 'tp' || msg === 'toilet paper'):
-			return 'go to are you interested in tp screen';
-		case(msg === 'yes tp'):
-			return 'go to choose tp description screen';
-		case(msg === 'fun'):
-			return 'go to fun tp screen';
-		case(msg === 'place order'):
-			return 'go to should i send to mott screen';
-		case(msg === 'no mott'):
-			return 'go to where should i send it screen';
-		case(msg === 'yes broad'):
-			return 'go to payment conf screen';
-		case(msg === 'yes payment'):
-			return 'go to order conf screen';
-	}
-
-    // switch(request.text) {
-    //     case 'hi':
-    //         return doWelcome();
-    //     case 'I need cat food':
-    //         return doINeedCatFood();
-    //     case 'I need tp':
-    //         return doINeedTp();
-    //     case 'I need dolphin print':
-    //         return doINeedDolphinPrintTp();
-    //     case 'Order dolphin':
-    //         return doCheckoutDolphinPrintTp();
-    //     case 'Subscribe dolphin':
-    //     	return doQuantityDolphinPrintTp();
-    //     case '1':
-    //     	return doFrequencyDolphinPrintTp();
-    //     case 'Every month':
-    //     	return doCheckoutDolphinPrintTp();
-    //     case 'Pay dolphin':
-    //         return doReceiptDolphinPrintTp();
-    //     case 'My Orders':
-    //         return getOrders();
-    //     default:
-    //         return 'Sorry, I don\'t understand';
+  switch(true) {
+    case(msg === 'hi' || msg === 'hello'):
+      return doWelcome();
+    case(msg === 'tp' || msg === 'toilet paper'):
+      return 'Are you interestd in toilet paper?';
+    case(msg === 'yes'):
+      return doChooseTp();
+    case(msg === 'fun tp'):
+      return doFunTp();
+    case(msg === 'order tp'):
+      return 'Should I send it to 71 Mott St, New York, NY, 11122?';
+    case(msg === 'no'):
+      return doChooseShipping();
+    case(msg === 'yes broad'):
+      return 'go to payment conf screen';
+    case(msg === 'yes payment'):
+      return 'go to order conf screen';
+  }
 }, {
   platforms: ['facebook']
 });
